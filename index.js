f1314cf297c4d62ec939fb921c87f207d90ef2a2
meteora277 <justin.s.diaz@gmail.com>
2021-11-26 15:46:22 -0500

    defined sum logic

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+const sum = function(fromN, toN) {
 
-function sum(fromN, toN) {
+  if (fromN === toN) {
+
+    return fromN;
+
+  } else {
+
+    return fromN + sum(fromN + 1, toN);
+
+  }
+
   // Sum all the values from fromN up to toN
-}
+};
 
 module.exports = sum;
